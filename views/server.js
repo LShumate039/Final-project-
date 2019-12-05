970cfdd0637e4b4301695ffb096781f2928ec349
lennye Shumate <LShuamte039@west-mec.org>
2019-12-04 18:18:22 -0700

    update 12/4

---
+++ views/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express()
+const port = 4000;
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+fetch(``)
